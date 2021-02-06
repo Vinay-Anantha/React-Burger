15423b3172267b4ac702b18cd162e0e02e94a5d3
Vinay <vinayanantha096@gmail.com>
2021-02-06 20:20:57 +0530

    display text when no ingredients are added

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,12 +2,19 @@ import React from "react";
 import cssObj from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
-  //transforming object prop to an array of jsx elements array
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, index) => {
-      return <BurgerIngredient key={igKey + index} type={igKey} />;
-    });
-  });
+  //transforming object prop to an array of arrays of jsx elements
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((prev, curr) => {
+      return prev.concat(curr);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
   return (
     <div className={cssObj.Burger}>
       <BurgerIngredient type="bread-top" />
