74cad23d8d539ca5ad78ff5c091ce362024ae80a
Vinay <vinayanantha096@gmail.com>
2021-02-06 16:35:26 +0530

    updated layout files

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import cssObj from "./Layout.module.css";
 const layout = (props) => (
   <React.Fragment>
     <div>Toolbar,SideDrawer,Backdrop</div>
-    <main>{props.children}</main>
+    <main className={cssObj.Content}>{props.children}</main>
   </React.Fragment>
 );
 
