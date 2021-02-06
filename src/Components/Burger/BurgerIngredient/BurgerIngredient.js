3ec705b9eccbb204f08f2056f9f5c3cb51693e70
Vinay <vinayanantha096@gmail.com>
2021-02-06 16:36:07 +0530

    added BurgerIngredient files

---
+++ src/Components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import cssObj from "./BurgerIngredient.module.css";
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={cssObj.BreadBottom}></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={cssObj.BreadTop}>
+            <div className={cssObj.Seeds1}></div>
+            <div className={cssObj.Seeds2}></div>
+          </div>
+        );
+        break;
+      case "meat1":
+        ingredient = <div className={cssObj.Meat1}></div>;
+        break;
+      case "meat2":
+        ingredient = <div className={cssObj.Meat2}></div>;
+        break;
+      case "salad":
+        ingredient = <div className={cssObj.Salad}></div>;
+        break;
+      case "cheese":
+        ingredient = <div className={cssObj.Cheese}></div>;
+        break;
+      default:
+        ingredient = null;
+        break;
+    }
+    return ingredient;
+  }
+}
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+export default BurgerIngredient;
