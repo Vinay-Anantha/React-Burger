9666945c8310bc7f35b2a6bacd8a67bc56190fc4
Vinay <vinayanantha096@gmail.com>
2021-02-06 20:20:13 +0530

    updated values

--- src/Container/BurgerBuilder/BurgerBuilder.js
+++ src/Container/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,10 @@ import Burger from "../../Components/Burger/Burger";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      meat2: 1,
-      cheese: 2,
-      meat1: 2
+      salad: 0,
+      meat2: 0,
+      cheese: 0,
+      meat1: 0
     }
   };
   render() {
