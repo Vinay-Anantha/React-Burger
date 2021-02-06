814cb8cf0cae5e4e7a35cc9a75ef1cbac1f5a256
Vinay <vinayanantha096@gmail.com>
2021-02-06 21:53:18 +0530

    Updated file

--- src/Container/BurgerBuilder/BurgerBuilder.js
+++ src/Container/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../Components/Burger/Burger";
+import BuildControls from "../../Components/Burger/BuildControls/BuildControls.js";
+
+const INGREDIENT_PRICE = {
+  salad: 10,
+  meat2: 40,
+  cheese: 20,
+  meat1: 30
+};
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -7,13 +15,56 @@ class BurgerBuilder extends Component {
       meat2: 0,
       cheese: 0,
       meat1: 0
+    },
+    totalPrice: 100
+  };
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+
+    const amountToAdd = INGREDIENT_PRICE[type];
+    const oldTotalPrice = this.state.totalPrice;
+    const newTotalPrice = oldTotalPrice + amountToAdd;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newTotalPrice
+    });
+  };
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
     }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+
+    const amountToSub = INGREDIENT_PRICE[type];
+    const oldTotalPrice = this.state.totalPrice;
+    const newTotalPrice = oldTotalPrice - amountToSub;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newTotalPrice
+    });
   };
   render() {
+    const disabledInfo = { ...this.state.ingredients };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientsAdded={this.addIngredientHandler}
+          ingredientsRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </React.Fragment>
     );
   }
