26d54d26faf504cf4cb42aceec7f4ef81f73fd9e
Vinay <vinayanantha096@gmail.com>
2021-02-06 16:35:49 +0530

    added burger files

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,12 @@
+import React from "react";
+import cssObj from "./Burger.Module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+const burger = () => {
+  return (
+    <div className={cssObj.Burger}>
+      <div></div>
+    </div>
+  );
+};
+
+export default burger;
