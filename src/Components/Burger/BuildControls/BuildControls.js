dd2d7e7d4c54e4c426d79b2fc86940929a473cab
Vinay <vinayanantha096@gmail.com>
2021-02-06 21:52:33 +0530

    added BuildControls

---
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,40 @@
+import React from "react";
+import cssObj from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+const controls = [
+  {
+    label: "Salad",
+    type: "salad"
+  },
+  {
+    label: "Meat1",
+    type: "meat1"
+  },
+  {
+    label: "Meat2",
+    type: "meat2"
+  },
+  {
+    label: "Cheese",
+    type: "cheese"
+  }
+];
+const buildControls = (props) => (
+  <div className={cssObj.BuildControls}>
+    <p>
+      Current Price : <strong>₹{props.price}</strong>{" "}
+    </p>
+    {controls.map((ctrl) => {
+      return (
+        <BuildControl
+          ingredientLabel={ctrl.label}
+          key={ctrl.label}
+          added={() => props.ingredientsAdded(ctrl.type)}
+          removed={() => props.ingredientsRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      );
+    })}
+  </div>
+);
+export default buildControls;
