9261902a19e45b36073339d14f37986ab0ec540c
Vinay <vinayanantha096@gmail.com>
2021-02-08 02:18:41 +0530

    Performance upgrade

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import cssObj from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
-const Modal = (props) => (
-  <React.Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={cssObj.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+
+//implemnting shouldComponentUpdate to improve performance.
+//this also prevents OrderSummary from getting rendered when Modal is not visible in BurgerBuilder
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={cssObj.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
 export default Modal;
