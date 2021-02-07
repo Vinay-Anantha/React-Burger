27d9f48f8bdefa88641f70f026be1a99078f02ea
Vinay <vinayanantha096@gmail.com>
2021-02-08 01:50:38 +0530

    Hamburger toggle

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -2,9 +2,10 @@ import React from "react";
 import cssObj from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../../Navigation/SideDrawer/DrawerToggle/DrawerToggle";
 const toolbar = (props) => (
   <header className={cssObj.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={cssObj.Logo}>
       <Logo />
     </div>
