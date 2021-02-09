976606edacd57e260afad68c69f25d6c249f933a
Vinay <vinayanantha096@gmail.com>
2021-02-10 02:36:24 +0530

    passing references for buttons

--- src/Components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/Components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,10 +8,10 @@ const checkoutSummary = (props) => {
       <h1>We hope you love it!!</h1>
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
-        <Button btnType="Danger" clicked>
+        <Button btnType="Danger" clicked={props.checkoutCancelled}>
           CANCEL
         </Button>
-        <Button btnType="Success" clicked>
+        <Button btnType="Success" clicked={props.checkoutContinued}>
           CONTINUE
         </Button>
       </div>
