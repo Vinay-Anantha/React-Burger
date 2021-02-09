55e3b8ca41036d7ad652842f29b07b6c6e1a7c7d
Vinay <vinayanantha096@gmail.com>
2021-02-10 02:35:47 +0530

    setting state from query param values

--- src/Container/Checkout/Checkout.js
+++ src/Container/Checkout/Checkout.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import CheckoutSummary from "../../Components/Order/CheckoutSummary/CheckoutSummary";
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1
-    }
+    ingredients: {}
   };
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+
+    const ingredients = {};
+    for (let param of query.entries()) {
+      //["salsa","1"]
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
