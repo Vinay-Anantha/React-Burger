202604d067723ae6eb55bcddf07de00f02e0fb44
Vinay <vinayanantha096@gmail.com>
2021-02-14 14:35:57 +0530

    Added provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import "./index.css";
 import App from "./App";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/React-Burger">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/React-Burger">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
